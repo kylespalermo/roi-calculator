55363e084b2d6b4874ca571f68e808e3706f3d57
Kyle Palermo <61397896+kylespalermo@users.noreply.github.com>
2024-02-02 10:04:10 -0500

    Create roi-calculator.js

---
+++ roi-calculator.js
@@ -0,0 +1,18 @@
+// "#fivetran-contract-period"
+// "#est-onboarding-time"
+// "#average-salary"
+// "#equivalent-hourly-rate"
+// "#hours-worked-per-week"
+// "#weeks-worked-per-year"
+
+const defaultSalary = 70000;
+
+const fivetranContractPeriod = document.getElementById("fivetran-contract-period")
+const estOnboardingTime = document.getElementById("est-onboarding-time")
+const averageSalary = document.getElementById("average-salary")
+const equivalentHourlyRate = document.getElementById("equivalent-hourly-rate")
+const hoursWorkedPerWeek = document.getElementById("hours-worked-per-week")
+const weeksWorkedPerYear = document.getElementById("weeks-worked-per-year")
+
+
+averageSalary.defaultValue = defaultSalary;
